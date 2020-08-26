9bece2986be24971021183cef18dfc49b423ab4d
Kaio Vitor Morsch <kakamorsch@gmail.com>
2020-08-25 21:22:33 -0300

    adding the computer movements after every player move

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -2,6 +2,7 @@ defmodule ExMon do
   alias ExMon.{Game, Player}
   alias ExMon.Game.{Status, Actions}
   @computer_name "Dilma"
+  @computer_moves [:move_avg, :move_rnd, :move_heal]
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
   end
@@ -11,21 +12,39 @@ defmodule ExMon do
     |> create_player(:xingar, :mentir, :inflacionar)
     |> Game.start(player)
 
-    Status.print_round_message()
+    Status.print_round_message(Game.info())
   end
 
   def make_move(move) do
+    Game.info()
+    |> Map.get(:status)
+    |> handle_status(move)
+  end
+
+  defp handle_status(:game_over, _move), do: Status.print_round_message(Game.info())
+
+  defp handle_status(_other, move) do
     move
     |> Actions.fetch_move()
     |> do_move
-  end
 
+    computer_move(Game.info())
+  end
   def do_move({:error, move}), do: Status.print_wrong_move_message(move)
 
   def do_move({:ok, move}) do
     case move do
-      :move_heal -> "realiza_cura"
+      :move_heal -> Actions.heal()
       move -> Actions.attack(move)
     end
+
+    Status.print_round_message(Game.info())
   end
+
+  defp computer_move(%{turn: :computer, status: :continue}) do
+    move = {:ok, Enum.random(@computer_moves)}
+    do_move(move)
+  end
+
+  defp computer_move(_), do: :ok
 end
